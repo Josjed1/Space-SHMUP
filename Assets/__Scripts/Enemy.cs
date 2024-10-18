deaba1d3ce8a43495b27e5cf382a931f7df0ad07
Josjed1 <jocaley@siue.edu>
2024-10-18 15:27:48 -0500

    Pushing Final Code

---
+++ Assets/__Scripts/Enemy.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Inscribed")]
+    public float speed = 10f;
+    public float fireRate = .3f;
+    public float health = 10;
+    public float score = 100;
+
+
+    protected BoundsCheck bndCheck;
+
+    void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+    public Vector3 pos
+    {
+        get
+        {
+            return this.transform.position;
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        if(bndCheck.LocIs(BoundsCheck.eScreenLocs.offDown))
+        {
+            Destroy(gameObject);            
+        }
+    }
+
+    public virtual void Move()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject otherGO = coll.gameObject;
+        if (otherGO.GetComponent<Projectile_Hero>() != null)
+        {
+            Destroy(otherGO);
+            Destroy(gameObject);
+        }
+        else
+        {
+            Debug.Log("Enemy hit by non-ProjectileHero: " + otherGO.name);
+        }
+    }
+
+    // void OnCollisionEnter(Collision coll)
+    // {
+    //     GameObject otherGO = coll.gameObject;
+    //     Projectile_Hero p = otherGO.GetComponent<Projectile_Hero>();
+    //     if (p != null)
+    //     {
+    //         if(bndCheck.isOnScreen)
+    //         {
+    //             health -= Main.GET_WEAPON_DEFINITION(p.type).damageOnHit;
+    //             if (health <= 0)
+    //             {
+    //                 Destroy(this.gameObject);
+    //             }
+    //         }
+    //         Destroy(otherGO);
+    //     }
+    //     else
+    //     {
+    //         print("Enemy hit by non-ProjectileHero: " + otherGO.name);
+    //     }
+    // }
+}
---
+++ Assets/__Scripts/Hero.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    static public Hero          S{get; private set;}
+    
+    [Header("Inscribed")]
+    public float                speed = 30;
+    public float                rollMult = -45;
+    public float                pitchMult = 30;
+    public GameObject           projectilePrefab;
+    public float                projectileSpeed = 40;
+
+    [Header("Dynamic")] [Range(0,4)]
+    private float _shieldLevel = 1;
+
+    [Tooltip("This filed holds a reference to the last triggering GameObject")]
+    private GameObject lastTriggerGo = null;
+
+    public delegate void WeaponFireDelegate();
+    public event WeaponFireDelegate fireEvent;
+
+
+
+    void Awake()
+    {
+        if (S == null)
+        {
+            S = this;
+        }
+        else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
+        // fireEvent += TempFire;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += hAxis * speed * Time.deltaTime;
+        pos.y += vAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        transform.rotation = Quaternion.Euler(vAxis*pitchMult,hAxis*rollMult, 0);
+
+        if (Input.GetAxis("Jump") == 1 && fireEvent != null)
+        {
+            fireEvent();
+        }
+    }
+
+    // void TempFire()
+    // {
+    //     GameObject projGO = Instantiate<GameObject>(projectilePrefab);
+    //     projGO.transform.position = transform.position;
+    //     Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
+
+    //     Projectile_Hero proj = projGO.GetComponent<Projectile_Hero>();
+    //     proj.type = eWeaponType.blaster;
+    //     float tSpeed = Main.GET_WEAPON_DEFINITION(proj.type).velocity;
+    //     rigidB.velocity = Vector3.up * tSpeed;
+    // }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+        
+        if(go == lastTriggerGo) return;
+        lastTriggerGo = go;
+
+        Enemy enemy = go.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            shieldLevel--;
+            Destroy(go);
+        }
+        else
+        {
+            Debug.LogWarning("Shield trigger hit by non enemy");
+        }
+    }
+
+    public float shieldLevel
+    {
+        get {return (_shieldLevel);}
+        private set {
+            _shieldLevel = Mathf.Min(value, 4);
+            if(value < 0)
+            {
+                Destroy(this.gameObject);
+                Main.HERO_DIED();
+            }
+        }
+    }
+}
